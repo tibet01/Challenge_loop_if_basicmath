a9e27182f02323a786d04522ae9d101dd3d45eda
tibet01 <tibet_rintharak@hotmail.co.th>
2020-08-10 17:05:16 +0700

    Challenge1

--- Challenge_loop_if_basicmath/Challenge1.cpp
+++ Challenge_loop_if_basicmath/Challenge1.cpp
@@ -1,5 +1,40 @@
 #include<stdio.h>
+#include<math.h>
+#include<string.h>
 int main()
 {
-	return 0;
+    long int a, b, n, t;
+    double avg, sum = 0, sum2 = 0, sd;
+    scanf("%ld %ld", &a, &b);
+    n = fabs(a - b) + 1;
+    if (a < b)
+    {
+        for (long int i = a; i <= b; i++)
+        {
+            printf("%ld ", i);
+            sum += i;
+        }
+    }
+    else
+    {
+        for (long int i = a; i >= b; i--)
+        {
+            printf("%ld ", i);
+            sum += i;
+        }
+    }
+    avg = sum / n;
+    printf("\nAverage = %.1lf\n", avg);
+    if (a > b)
+    {
+        t = a;
+        a = b;
+        b = t;
+    }
+    for (long int i = a; i <= b; i++)
+    {
+        sum2 = sum2 + pow(i - avg, 2);
+    }
+    printf("SD = %.2lf", sqrt(sum2 / (n - 1)));
+    return 0;
 }
